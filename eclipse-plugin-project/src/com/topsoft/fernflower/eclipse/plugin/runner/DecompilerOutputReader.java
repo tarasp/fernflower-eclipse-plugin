1c3b8457aeeff656e31190e78aa64b37644c8534
tarasp <fernflower.eclipse.plugin@gmail.com>
2015-01-27 10:53:04 +0200

    Reformatted sources

--- eclipse-plugin-project/src/com/topsoft/fernflower/eclipse/plugin/runner/DecompilerOutputReader.java
+++ eclipse-plugin-project/src/com/topsoft/fernflower/eclipse/plugin/runner/DecompilerOutputReader.java
@@ -10,51 +10,39 @@ import java.util.List;
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.ui.console.MessageConsoleStream;
 
-public class DecompilerOutputReader
-  implements Runnable
-{
-  private BufferedReader reader;
-  private MessageConsoleStream consoleStream;
-  private String command;
-  
-  public DecompilerOutputReader(List<String> command, InputStream inputStream, MessageConsoleStream consoleStream)
-  {
-    this.command = StringUtils.join(command, ' ');
-    this.reader = new BufferedReader(new InputStreamReader(inputStream));
-    this.consoleStream = consoleStream;
-  }
-  
-  public void run()
-  {
-    try
-    {
-      String line = this.reader.readLine();
-      this.consoleStream.println(this.command);
-      while (line != null)
-      {
-        this.consoleStream.println(line);
-        line = this.reader.readLine();
-      }
-      this.consoleStream.println("Done");
-      LoggerUtil.logDebug("Decompiler execution is finished..");
-      close(this.reader);
-      close(this.consoleStream);
-    }
-    catch (IOException e)
-    {
-      LoggerUtil.logError("Error reading decompiler output.", e);
-    }
-  }
-  
-  private void close(Closeable stream)
-  {
-    try
-    {
-      stream.close();
-    }
-    catch (IOException e)
-    {
-      LoggerUtil.logError("Error closing " + stream.getClass().getSimpleName(), e);
-    }
-  }
+public class DecompilerOutputReader implements Runnable {
+	private BufferedReader reader;
+	private MessageConsoleStream consoleStream;
+	private String command;
+
+	public DecompilerOutputReader(List<String> command, InputStream inputStream, MessageConsoleStream consoleStream) {
+		this.command = StringUtils.join(command, ' ');
+		this.reader = new BufferedReader(new InputStreamReader(inputStream));
+		this.consoleStream = consoleStream;
+	}
+
+	public void run() {
+		try {
+			String line = reader.readLine();
+			consoleStream.println(command);
+			while (line != null) {
+				consoleStream.println(line);
+				line = reader.readLine();
+			}
+			consoleStream.println("Done");
+			LoggerUtil.logDebug("Decompiler execution is finished..");
+			close(reader);
+			close(consoleStream);
+		} catch (IOException e) {
+			LoggerUtil.logError("Error reading decompiler output.", e);
+		}
+	}
+
+	private void close(Closeable stream) {
+		try {
+			stream.close();
+		} catch (IOException e) {
+			LoggerUtil.logError("Error closing " + stream.getClass().getSimpleName(), e);
+		}
+	}
 }
